b4b9f6654e451bf32a491b5d30534d135839d733
Amine Badraoui <aminebadraoui94@gmail.com>
2025-01-24 16:57:54 -0500

    fresh start

--- app/core/__init__.py
+++
@@ -1,1 +0,0 @@
-# This can be empty, it's just to make the directory a Python package 
--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from ..db.base_class import Base
-from .user import User  # This ensures all models are imported for Alembic
-
-__all__ = ['Base', 'User'] 
